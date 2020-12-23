9f65d1c19455d60738e2773b8d18b2252fb8a24c
Naz Kolcu <nazklc@gmail.com>
2020-12-23 22:14:39 +0300

    2. deneme

--- src/org/kodluyoruz/Main.java
+++ src/org/kodluyoruz/Main.java
@@ -3,6 +3,6 @@ package org.kodluyoruz;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("deneme !!!");
+        System.out.println("deneme2 !!!");
     }
 }
